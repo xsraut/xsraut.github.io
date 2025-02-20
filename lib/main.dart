eaa9b76ac143eb9772b56a641c88dd517d64bab8
xsraut <xsraut@gmail.com>
2025-02-20 05:52:34 +0530

    fix: Fixed exit full screen mode option button, by creating custom FAB using Html element

--- lib/main.dart
+++ lib/main.dart
@@ -39,10 +39,15 @@ class _HomePageState extends State<HomePage> {
   /// Whether the floating action button (FAB) menu is open.
   bool isMenuOpen = false;
 
+  bool isFullScreened = false;
+
   @override
   void initState() {
     super.initState();
     _registerImageElement();
+    html.document.addEventListener('fullscreenchange', (event){
+      fullScreenListner();
+    });
   }
 
   @override
@@ -52,19 +57,128 @@ class _HomePageState extends State<HomePage> {
     super.dispose();
   }
 
-  /// Registers an HTML `<img>` element for rendering inside the Flutter app.
-  void _registerImageElement() {
-    final html.ImageElement imgElement = html.ImageElement()
-      ..id = "img-element"
-      ..style.width = "100%"
-      ..style.height = "100%"
-      ..style.objectFit = 'contain'
-      ..onDoubleClick.listen((_) => _toggleFullscreen());
+  // HTML image element to show image via url
+  final html.ImageElement imageElement = html.ImageElement();
 
-    // Registers the HTML image element so Flutter can render it inside a widget.
+  // Html button used as Floating Action Button
+  final html.ButtonElement fabElement = html.ButtonElement();
+
+  // Maximize (image) button element
+  final html.ButtonElement maxBtnElement = html.ButtonElement();
+
+  // Minimize (image) button element
+  final html.ButtonElement minBtnElement = html.ButtonElement();
+
+  // Options div that holds maxBtnElement and minBtnElement in a column above fabElement
+  final html.DivElement optionsContainer = html.DivElement();
+
+  // Image container div
+  // When full screen is disabled, it only contins imageElement
+  // When full screen is enabled, it holds imageElement, fabElement and optionsContainer
+  final html.DivElement imageContainer = html.DivElement();
+
+  /// Registers an HTML `<img>` element for rendering inside the Flutter app.
+  /// It must be called in init() function
+  void _registerImageElement() {
+
+    imageElement
+      ..id = "img-element"
+      ..onDoubleClick.listen((_) => _toggleFullscreen()); // Toggle fullscreen when double clicked on image
+    
+    imageElement.style
+      ..width = "100%"
+      ..height = "100%"
+      ..objectFit = "contain";
+
+    fabElement
+      ..id = "fab-element"
+      ..text = "+"
+      ..onClick.listen((data) => _toggleMenu()); // Show/Hide fullscreen option menu
+    
+    fabElement.style
+      ..fontSize = "25px"
+      ..cursor = "pointer"
+      ..right = "10px"
+      ..bottom = "10px"
+      ..height = "60px"
+      ..width = "60px"
+      ..margin = "0"
+      ..position = "absolute"
+      ..border = "none"
+      ..borderRadius = "5px"
+      ..boxShadow = "0 2px 5px 0 rgba(0,0,0,0.2)"
+      ..backgroundColor = "white"
+      ..color = "violet";
+    
+    maxBtnElement.onClick.listen((data) => _enableFullScreen()); // maximize image when clicked
+
+    maxBtnElement
+      ..id = "max-btn"
+      ..text = "Enter Full Screen"
+      ..style.fontSize = "15px"
+      ..style.border = "none"
+      ..style.borderRadius = "5px"
+      ..style.height = "30px";
+    
+    minBtnElement.onClick.listen((data) => _disableFullScreen()); // minimize image when clicked
+    
+    minBtnElement
+      ..id = "min-btn"
+      ..text = "Exit Full Screen"
+      ..style.fontSize = "15px"
+      ..style.border = "none"
+      ..style.borderRadius = "5px"
+      ..style.height = "30px";
+
+    optionsContainer.children.add(maxBtnElement); // Add maxBtnElement inside optionsContainer
+    optionsContainer.children.add(minBtnElement); // Add minBtnElement inside optionsCOntainer
+
+    optionsContainer.style
+      ..display = "flex"
+      ..flexDirection = "column"
+      ..rowGap = "5px"
+      ..right = "10px"
+      ..bottom = "75px"
+      ..height = "60px"
+      ..width = "150px"
+      ..margin = "0"
+      ..position = "absolute";
+
+    imageContainer
+      ..id = "img-container"
+      ..children.add(imageElement);
+    
+    
+    // Register the HTML elements
+
     ui_web.platformViewRegistry.registerViewFactory(
       'imageElement',
-      (int viewId) => imgElement,
+      (int viewId) => imageElement,
+    );
+
+    ui_web.platformViewRegistry.registerViewFactory(
+      'fabElement',
+      (int viewId) => fabElement,
+    );
+
+    ui_web.platformViewRegistry.registerViewFactory(
+      'imageContainer',
+      (int viewId) => imageContainer,
+    );
+
+    ui_web.platformViewRegistry.registerViewFactory(
+      'optionsContainer',
+      (int viewId) => optionsContainer,
+    );
+
+    ui_web.platformViewRegistry.registerViewFactory(
+      'maxBtnElement',
+      (int viewId) => maxBtnElement,
+    );
+
+    ui_web.platformViewRegistry.registerViewFactory(
+      'minBtnElement',
+      (int viewId) => minBtnElement,
     );
   }
 
@@ -80,11 +194,12 @@ class _HomePageState extends State<HomePage> {
 
   /// Toggles fullscreen mode using JavaScript.
   void _toggleFullscreen() {
+
     js.context.callMethod('eval', [
       // js fucntion to toggle fullscreen
       '''
       if (!document.fullscreenElement) {
-        document.getElementById("img-element").requestFullscreen();
+        document.getElementById("img-container").requestFullscreen();
       } else {
         document.exitFullscreen();
       }
@@ -92,9 +207,45 @@ class _HomePageState extends State<HomePage> {
     ]);
   }
 
+
+  /// This function is called whenever the fullscreen mode is changed
+  /// This function changes [isFullScreened] to show/hide the floating action button 
+  /// over the html image element in [imageContainer] in full screen mode
+  /// When in enabling fullscreen mode, it adds [fabElement] in [imageContainer]
+  /// When fullscreen mode is disabled, it removes [fabElement] and/or [optionsContainer]
+  void fullScreenListner(){
+    setState(() {
+      isFullScreened = !isFullScreened;
+    });
+    
+    if(imageContainer.children.contains(fabElement)){
+      imageContainer.children.remove(fabElement); 
+    }else{
+      imageContainer.children.add(fabElement);
+    }
+
+    if(imageContainer.children.contains(optionsContainer)){
+      imageContainer.children.remove(optionsContainer);
+    }
+  }
+
+  /// Function to enable fullscreen mode using [maxBtnElement]
+  void _enableFullScreen(){
+    if(!isFullScreened) _toggleFullscreen();
+  }
+
+  /// Function to enable fullscreen mode using [minBtnElement]
+  void _disableFullScreen(){
+    if(isFullScreened) _toggleFullscreen();
+  }
+
   /// Toggles the visibility of the floating action button (FAB) menu.
   /// by setting [isMenuOpen] true/false 
   void _toggleMenu() {
+    if(isFullScreened){
+      isMenuOpen ? imageContainer.children.remove(optionsContainer) : imageContainer.children.add(optionsContainer);
+      return;
+    }
     setState(() {
       isMenuOpen = !isMenuOpen;
     });
@@ -126,7 +277,7 @@ class _HomePageState extends State<HomePage> {
                     child: AspectRatio(
                       aspectRatio: 1,
                       // Shows HTML image inside flutter widget
-                      child: HtmlElementView(viewType: 'imageElement'),
+                      child: HtmlElementView(viewType: 'imageContainer'),
                     ),
                   ),
                   const SizedBox(height: 8),
@@ -155,14 +306,21 @@ class _HomePageState extends State<HomePage> {
                 ],
               ),
             ),
-            floatingActionButton: FloatingActionButton(
-              onPressed: _toggleMenu,
-              child: const Icon(Icons.add),
-            ),
           ),
 
+          if(!isFullScreened) 
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child:SizedBox(
+                width: 50,
+                height: 50,
+                child: HtmlElementView(viewType: 'fabElement')
+              ),
+            ),
+
           // Dimmed background when the menu is open.
-          if (isMenuOpen)
+          if (!isFullScreened && isMenuOpen)
             Positioned.fill(
               child: GestureDetector(
                 onTap: _closeMenu, // Close menu when tapping outside.
@@ -173,44 +331,22 @@ class _HomePageState extends State<HomePage> {
             ),
 
           // Floating action button (FAB) menu.
-          if (isMenuOpen)
+          if (!isFullScreened && isMenuOpen)
             Positioned(
-              bottom: 80,
-              right: 16,
+              bottom: 0,
+              right: 0,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
-                  _menuOption("Enter Fullscreen", _toggleFullscreen),
-                  _menuOption("Exit Fullscreen", (){}),
+                  SizedBox(
+                    width: 100,
+                    height: 25,
+                    child: HtmlElementView(viewType: 'optionsContainer')
+                  ),
                 ],
               ),
             ),
         ],
-      ),
-    );
-  }
-
-  /// Creates a menu option button with a given [label] and [onTap] callback.
-  Widget _menuOption(String label, VoidCallback onTap) {
-    return GestureDetector(
-      onTap: () {
-        onTap();
-        _closeMenu();
-      },
-      child: Container(
-        margin: const EdgeInsets.only(bottom: 8),
-        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-        decoration: BoxDecoration(
-          color: const Color.fromARGB(255, 225, 216, 237),
-          borderRadius: BorderRadius.circular(8),
-        ),
-        child: SizedBox(
-          width: 120,
-          child: Text(
-            label, 
-            style: const TextStyle(fontSize: 16, inherit: false)
-          )
-        ),
       ),
     );
   }
